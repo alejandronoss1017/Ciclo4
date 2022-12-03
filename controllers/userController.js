1b9d6cda1aa2861c38764a29e819881ca10ad66e
Camilo Nossa <alejandro_noss@hotmail.com>
2022-12-03 17:21:00 -0500

    Creacion de metodos asincronicos para cada una de los endpoints establecidos

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,95 @@
-export function createUser(req, res) {
-  res.send("Hello World!, you gonna create a user");
-  res.status(200);
-  console.log("HTTP POST REQUEST FROM: ../user/");
+import userModel from "../models/user.js";
+
+export async function createUser(req, res) {
+  const user = req.body;
+  let document = null;
+
+  try {
+    document = await userModel.create(user);
+  } catch (error) {
+    res.status(400);
+    res.json(error.message);
+    return;
+  }
+  res.status(201);
+  res.json(document);
+  console.log("HTTP POST REQUEST FROM: ../user/new");
+
   return;
 }
-export function readUser(req, res) {
-  res.send("Hello World!, you gonna read a user");
+
+export async function readUser(req, res) {
+  const { name, lastName } = req.params;
+  let document = null;
+  try {
+    document = await userModel.find({
+      name,
+      lastName,
+    });
+  } catch (error) {
+    res.status(400);
+    res.json(error.message);
+    return;
+  }
+
   res.status(200);
-  console.log("HTTP GET REQUEST FROM: ../user/");
+  res.json(document);
+  console.log(`HTTP GET REQUEST FROM: ../user/${name}/${lastName}`);
   return;
 }
-export function updateUser(req, res) {
-  res.send("Hello World!, you gonna update a user");
+
+export async function updateUser(req, res) {
+  const { id } = req.params;
+  const { newValues } = req.body;
+  let document = null;
+
+  try {
+    document = await userModel.updateOne({ _id: id }, newValues);
+  } catch (error) {
+    res.status(400);
+    res.json(error.message);
+    return;
+  }
+
   res.status(200);
-  console.log("HTTP PUT REQUEST FROM: ../user/");
+  res.json(document);
+  console.log(`HTTP PUT REQUEST FROM: ../user/update/${id}`);
   return;
 }
-export function deleteUser(req, res) {
-  res.send("Hello World!, you gonna delete a user");
+
+export async function patchUser(req, res) {
+  const { id } = req.params;
+  const { newValues } = req.body;
+  let document = null;
+
+  try {
+    document = await userModel.updateOne({ _id: id }, newValues);
+  } catch (error) {
+    res.status(400);
+    res.json(error.message);
+    return;
+  }
+
   res.status(200);
+  res.json(document);
+  console.log(`HTTP PUT REQUEST FROM: ../user/patch/${id}`);
+  return;
+}
+
+export async function deleteUser(req, res) {
+  const { id } = req.params;
+  let document = null;
+
+  try {
+    document = await userModel.findByIdAndDelete(id);
+  } catch (error) {
+    res.status(400);
+    res.json(error.message);
+    return;
+  }
+
+  res.status(200);
+  res.json(document);
   console.log("HTTP DELETE REQUEST FROM: ../user/");
   return;
 }
