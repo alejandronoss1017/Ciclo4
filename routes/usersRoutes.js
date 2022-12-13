6bd481f09a9ea09edd93859f9ea775d8e4faca65
Camilo Nossa <alejandro_noss@homail.com>
2022-12-12 21:37:21 -0500

    Agregacion de los diferentes endpoints de la aplicacion

---
+++ routes/usersRoutes.js
@@ -0,0 +1,32 @@
+import express from "express";
+import {
+  createUser,
+  deleteUser,
+  readUser,
+  updateUser,
+} from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.post("/new", (req, res) => {
+  createUser(req, res);
+
+});
+
+userRouter.get("/:id", (req, res) => {
+  readUser(req, res);
+});
+
+userRouter.put("/:id", (req, res) => {
+  updateUser(req, res);
+});
+
+userRouter.patch("/:id", (req, res) => {
+  updateUser(req, res);
+});
+
+userRouter.delete("/:id", (req, res) => {
+  deleteUser(req, res);
+});
+
+export default userRouter;
