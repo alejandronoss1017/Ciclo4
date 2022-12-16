745881d15f3efe9b2ffaed061b856973c9a96a15
Camilo Nossa <alejandro_noss@hotmail.com>
2022-12-16 18:47:00 -0500

    Agregacion de las validaciones a los usuarios

--- models/user.js
+++ models/user.js
@@ -4,12 +4,28 @@ const userSchema = mongoose.Schema(
   {
     name: { type: String, require: true },
     lastName: { type: String, require: true },
-    age: { type: Number, require: true },
-    career: { type: String, require: true },
-    email: { type: String, require: true },
+    age: { type: Number, require: true, min: 16 },
+    career: {
+      type: String,
+      require: true,
+      enum: [
+        "Computer Science",
+        "Bussines Administrator",
+        "Medicine",
+        "Law",
+        "Art",
+      ],
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    },
     password: { type: String, require: true },
   },
-  { timeStamps: true }
+  { timestamps: true }
 );
 
 export default mongoose.model("users", userSchema);
